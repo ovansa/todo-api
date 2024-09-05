2ffdc27376214ba79b92718f22ae2027096dbb50
Muhammed Ibrahim <aminmuhammad18@gmail.com>
2024-09-05 16:19:31 +0100

    chore: Setup remote mongo url

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,17 @@ import express, { Application } from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import errorResponse from './middleware/error';
 
 import router from './routes';
 import logger from './utils/logger';
 
-const MONGO_URL = `mongodb://localhost:27017/todo-api`;
+dotenv.config();
+
+const MONGO_DEFAULT = `mongodb://localhost:27017/todo-api`;
+let MONGO_URL = process.env.MONGO_URL || MONGO_DEFAULT;
+logger.info(`process ${process.env.MONGO_URL}`);
 const app: Application = express();
 
 app.use(
@@ -19,6 +24,11 @@ app.use(
 app.use(express.json());
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('combined'));
+  MONGO_URL = MONGO_DEFAULT;
+}
+
+if (process.env.NODE_ENV === 'test') {
+  MONGO_URL = MONGO_DEFAULT;
 }
 
 const server = app.listen(3000, () => {
@@ -26,7 +36,10 @@ const server = app.listen(3000, () => {
 });
 
 mongoose.Promise = Promise;
-mongoose.connect(MONGO_URL).then(() => logger.info('DB Connected.'));
+
+mongoose
+  .connect(MONGO_URL)
+  .then(() => logger.info(`DB Connected. ${new URL(MONGO_URL).host}`));
 mongoose.connection.on(`error`, (error: Error) => logger.error(error));
 
 app.use('/', router());
